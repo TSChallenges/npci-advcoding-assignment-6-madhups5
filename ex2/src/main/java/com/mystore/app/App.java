acced0eaaadad20ca80dbe45472e0de2ea226c40
madhups5 <paidasubramanyammadhu5@gmail.com>
2025-02-09 00:37:27 +0530

    Refactored code to use Spring Framework with @Configuration and @Bean

--- ex2/src/main/java/com/mystore/app/App.java
+++ ex2/src/main/java/com/mystore/app/App.java
@@ -1,16 +1,18 @@
 package com.mystore.app;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 public class App {
     public static void main(String[] arg) {
-        
-        Product p1 = new Product();
-        p1.setId(1);
-        p1.setName("Tumbler");
-        System.out.println(p1);
 
-        Product p2 = new Product();
-        p2.setId(2);
-        p2.setName("Bucket");        
-        System.out.println(p2);
+        // Load the Spring Application Context
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // Retrieve Product beans from Spring's IoC container
+        Product tumbler = context.getBean("tumbler", Product.class);
+        Product bucket = context.getBean("bucket", Product.class);
+
+        // Display the Product details
+        System.out.println("Product 1: " + tumbler);
+        System.out.println("Product 2: " + bucket);
     }  
 }
