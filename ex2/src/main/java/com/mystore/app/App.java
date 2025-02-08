63b02df3670f6006179fbcfef9d1d2ae3979b473
madhups5 <paidasubramanyammadhu5@gmail.com>
2025-02-09 01:06:38 +0530

    Completed playground challenge

--- ex2/src/main/java/com/mystore/app/App.java
+++ ex2/src/main/java/com/mystore/app/App.java
@@ -1,18 +1,19 @@
 package com.mystore.app;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App {
     public static void main(String[] arg) {
-
-        // Load the Spring Application Context
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Product p1 = context.getBean("tumbler",Product.class);
+        p1.setId(1);
+        p1.setName("Tumbler");
+        System.out.println(p1);
 
-        // Retrieve Product beans from Spring's IoC container
-        Product tumbler = context.getBean("tumbler", Product.class);
-        Product bucket = context.getBean("bucket", Product.class);
-
-        // Display the Product details
-        System.out.println("Product 1: " + tumbler);
-        System.out.println("Product 2: " + bucket);
+        Product p2 = context.getBean("bucket",Product.class);
+        p2.setId(2);
+        p2.setName("Bucket");
+        System.out.println(p2);
     }  
 }
